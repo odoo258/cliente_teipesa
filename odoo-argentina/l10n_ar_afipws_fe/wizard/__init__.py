87b9455bd22b0bb7354f7570ff5b5a48d4cb9328
Key <keyla@moogah.com>
2018-10-31 11:46:37 -0300

    removed some files from odoo-argentina

--- odoo-argentina/l10n_ar_afipws_fe/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import afip_ws_consult_wizard
-from . import afip_ws_currency_rate_wizard
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
