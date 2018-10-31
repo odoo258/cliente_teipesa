9e9bcbdfa5acae7d409f1d34c6d1dee7f1e7b999
Key <keyla@moogah.com>
2018-10-31 11:48:33 -0300

    removed odoo-argentina

--- odoo-argentina/l10n_ar_account/report/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-# TODO arreglar e incorporar
-# from . import invoice_analysis
